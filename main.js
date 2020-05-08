02a1ee26c1e241dae96ff5e6ad6ed191bb20ba04
Jahoob <jpbartosiewicz@gmail.com>
2020-05-08 11:22:14 +0200

    fixed menu, added new sections

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 (function ($) {
-    "use strict"; // Start of use strict
+    "use strict";
 
-    // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -15,22 +14,18 @@
         }
     });
 
-    // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function () {
         $('.navbar-collapse').collapse('hide');
     });
 
-    // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
         target: '#mainNav',
         offset: 100
     });
 
-    // Collapse Navbar
     var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
-            // tak kurwa lapac i zminiac style albo dodawac klase ktora to zmienia jak wyzej
             $('.navbar').css("background-color", "black")
             $('.navbar-brand').addClass("color-change")
         } else {
@@ -39,9 +34,7 @@
             $('.navbar').removeClass("color-change")
         }
     };
-    // Collapse now if page is not at top
     navbarCollapse();
-    // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 
-})(jQuery); // End of use strict
+})(jQuery); 
